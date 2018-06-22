36f75a3d8b9f2213f9bf85ee8a9bbfe0a5d035ef
VincenzF <vincenz-frenzel@gmx.de>
2018-06-22 09:31:04 -0400

    created startup script and install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# rqt_gcs_gui requires rqt_gui
+sudo apt-get install ros-kinetic-rqt ros-kinetic-rqt-common-plugins 
+
+# bebop_auto requires robot_upstart
+sudo apt-get install ros-kinetic-robot-upstart 
+

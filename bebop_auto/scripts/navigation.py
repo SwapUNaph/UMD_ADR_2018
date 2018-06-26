3e782df03a899b87047e33f4aa16a69ee8f41bd1
Vincenz Frenzel <V.Frenzel@gmx.net>
2018-06-26 15:13:57 -0400

    started working on odometry and blind path planner creates fake gate. Navigation reads paths and position and publishes empty command to driving. New message type added for this last message passing

--- bebop_auto/scripts/navigation.py
+++ bebop_auto/scripts/navigation.py
@@ -3,21 +3,64 @@
 #  --- Changelog ---
 # Goal:     Input of both path planners. Find relevant waypoint depending on current position. Use visual path if possible, otherwise use blind backup. Calculate driving command based on waypoint position and orientation. Create driving commands
 # Status:   06/19: Not existing
-#           06/25: Empty file
+#           06/26: Reads updates from blind path, visual path and position. Publishes empty command
 
+from geometry_msgs.msg import Pose, PoseArray, PoseStamped
+from std_msgs.msg import Int32
+# roslib.load_manifest('learning_tf')
 import rospy
-import roslaunch
+import tf
 import time
-from std_msgs.msg import Empty
-from subprocess import check_output
-from geometry_msgs.msg import Twist
-import pygame
-import signal
-import sys
-import time
-from bebop_msgs.msg import Ardrone3PilotingStateFlyingStateChanged
-from std_msgs.msg import Bool, Int32
+import numpy as np
+from bebop_auto.msg import Auto_Driving_Msg
+
+
+def received_update(data, args):
+    #print(drone_pos.position.x, drone_pos.position.y, drone_pos.position.z, drone_pos.orientation.x,
+    #      drone_pos.orientation.y, drone_pos.orientation.z, drone_pos.orientation.w)
+
+    global position
+    global path_visual
+    global path_blind
+
+    if args == "position":
+        position = data
+    elif args == "path_visual":
+        path_visual = data
+    elif args == "path_blind":
+        path_blind = data
+
+
+    print("send driving message")
+    msg = Auto_Driving_Msg()
+    msg.x = 0
+    msg.y = 0
+    msg.z = 0
+    msg.r = 0
+    publisher.publish(msg)
+
+
+def main():
+    rospy.init_node('navigation', anonymous=True)
+
+    global position
+    global path_visual
+    global path_blind
+    rospy.Subscriber("/auto/odometry_merged", Pose, received_update, "position")
+    rospy.Subscriber("/auto/path_blind", Pose, received_update, "path_blind")
+    rospy.Subscriber("/auto/path_visual", Pose, received_update, "path_visual")
+
+    global publisher
+    publisher = rospy.Publisher("/auto/auto_drive", Auto_Driving_Msg, queue_size=1)
+
+    rospy.spin()
 
 
 # read in position and path
 # if we are close enough to the last waypoint, advance status
+
+
+
+if __name__ == '__main__':
+    main()
+
--- bebop_auto/scripts/path_planner_blind.py
+++ bebop_auto/scripts/path_planner_blind.py
@@ -4,48 +4,74 @@
 # Goal:     Input current state and create a path using the information of the map and track layout. Used only if no gate was detected. Publishes path.
 # Status:   06/19: Not existing
 #           06/25: Takes in position and state
+#           06/26: Creates a fake gate location at the very first time it is started and keeps publishing this location as blind path. Transform not working correctly
 
-from __future__ import print_function
+
 from geometry_msgs.msg import Pose, PoseArray, PoseStamped
 from std_msgs.msg import Int32
 # roslib.load_manifest('learning_tf')
 import rospy
 import tf
+import time
+import numpy as np
 
 
 def position_updated(drone_pos):
-    print(drone_pos)
+    #print(drone_pos.position.x, drone_pos.position.y, drone_pos.position.z, drone_pos.orientation.x,
+    #      drone_pos.orientation.y, drone_pos.orientation.z, drone_pos.orientation.w)
 
     global initialized
     if not initialized:
-        #initialize with this position
+        print("initializing fake target")
+        print("Drone position: ")
+        print(drone_pos)
+        q = [drone_pos.orientation.x, drone_pos.orientation.y, drone_pos.orientation.z, drone_pos.orientation.w]
+        qi = q
+        qi[3] = -qi[3]
+
+        gate = [1, 0, 1, 0]
+        print("Virtual gate (local):")
+        print(gate[0:3])
+        gate_tf = tf.transformations.quaternion_multiply(qi, gate)
+        gate_tf = gate_tf.tolist()[0:3]
+        print("Virtual gate (global):")
+        print(gate_tf)
+
+        # initialize with this position
+        global target
+        target = gate_tf
         initialized = True
 
     if current_state == 4:
-        WP = [drone_pos.position.x, drone_pos.position.y, drone_pos.position.z]
-        # publisher.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        WP = Pose()
+        WP.x = target[0]
+        WP.y = target[1]
+        WP.z = target[2]
+
+        global publisher
+        publisher.publish(Pose)
 
 
 def state_updated(data):
     global current_state
+    global target
     current_state = data.data
 
 
 def main():
     rospy.init_node('path_planner_visual', anonymous=True)
 
+    global current_state
     current_state = -1
     rospy.Subscriber("/auto/state_machine", Int32, state_updated)
     rospy.Subscriber("/auto/odometry_merged", Pose, position_updated)
+    global publisher
     publisher = rospy.Publisher("/auto/path_blind", Pose, queue_size=2)
 
     global initialized
     initialized = False
 
     rospy.spin()
-
-    print("Shutting down")
-    # cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':

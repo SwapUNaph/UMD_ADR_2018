added files to tune visual masking. Minor bug fixes

scripts/gate_detection.py
@@ -29,80 +29,28 @@ import sys
 def signal_handler(signal, frame):
     sys.exit(0)
 
-#
-# def find_threshold_bimodal(array):
-#     array = np.array(array,dtype="double")
-#     array_sum = np.sum(array)
-#     var = []
-#     for n in range(array.size):
-#         partarray_sum = np.sum(array[:n])
-#         if partarray_sum == 0:
-#             s1 = 0
-#             var1 = 0
-#         else:
-#             s1 = np.sum(array[:n]) / array_sum
-#             mean1 = np.sum(array[:n] * range(n)) / partarray_sum
-#             var1 = np.sum(np.square(range(n) - mean1) * array[:n] / array_sum / s1)
-#
-#         partarray_sum = np.sum(array[n:])
-#         if partarray_sum == 0:
-#             s2 = 0
-#             var2 = 0
-#         else:
-#             s2 = np.sum(array[n:]) / array_sum
-#             mean2 = np.sum(array[n:] * range(n, array.size)) / partarray_sum
-#             var2 = np.sum(np.square(range(n, array.size) - mean2) * array[n:] / array_sum / s2)
-#         var.append(int(s1 * var1 + s2 * var2))
-#     idx = (var.index(min(var)) + len(var) - 1 - var[::-1].index(min(var)))/2
-#
-#     if idx >= 90:
-#         angle_thres = idx - 90
-#     else:
-#         angle_thres = idx
-#
-#     return angle_thres
-
-
-def isect_lines(line1, line2):
-    for x1, y1, x2, y2 in line1:
-        for x3, y3, x4, y4 in line2:
-            try:
-                s = float((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / float(
-                    (x4 - x3) * (y2 - y1) - (x2 - x1) * (y4 - y3))
-
-                x = x3 + s * (x4 - x3)
-                y = y3 + s * (y4 - y3)
-                return x, y
-
-            except ZeroDivisionError:
-                rospy.loginfo("ZeroDivisionError in isect_lines")
-                return -1, -1, -1
-
 
 def mask_image(hsv, color):
 
     # Threshold the HSV image to get only orange colors
-    # lower_color1 = np.array([0, 90, 80])  # orange 3D cypress         rgb
-    # upper_color1 = np.array([13, 255, 150])  # orange 3D cypress      rgb
-    # lower_color2 = np.array([170, 90, 80])  # orange 3D cypress       rgb
-    # upper_color2 = np.array([180, 255, 150])  # orange 3D cypress     rgb
-    # lower_color1 = np.array([0, 90, 80])  # orange matlab dynamic     rgb
-    # upper_color1 = np.array([15, 255, 255])  # orange matlab dynamic  rgb
-    # lower_color2 = np.array([160, 90, 80])  # orange matlab dynamic   rgb
-    # upper_color2 = np.array([180, 255, 255])  # orange matlab dynamic rgb
-
-    # from -10... 20
     if color == "orange":
-        # lower_color = np.array([85, 60, 80])  # orange matlab jungle     bgr
-        # upper_color = np.array([130, 255, 255])  # orange matlab jungle    bgr
+        # lower_color = np.array([85, 60, 80])  # orange matlab jungle
+        # upper_color = np.array([130, 255, 255])  # orange matlab jungle
 
-        lower_color = np.array([110, 80, 80])  # orange matlab dynamic     bgr
-        upper_color = np.array([130, 255, 255])  # orange matlab dynamic  bgr
+        # lower_color = np.array([110, 80, 80])  # orange matlab dynamic
+        # upper_color = np.array([130, 255, 255])  # orange matlab dynamic
+
+        lower_color = np.array([102, 90, 70])  # orange dynamic cypress
+        upper_color = np.array([117, 255, 195])  # orange dynamic cypress
 
         publisher = publisher_image_threshold_orange
     else:
-        lower_color = np.array([40, 100, 50])  # green matlab pointer
-        upper_color = np.array([90, 255, 255])  # green matlab pointer
+        # lower_color = np.array([40, 100, 50])  # green matlab pointer
+        # upper_color = np.array([90, 255, 255])  # green matlab pointer
+
+        lower_color = np.array([10, 80, 60])  # green cypress pointer
+        upper_color = np.array([25, 150, 180])  # green cypress pointer
+
         publisher = publisher_image_threshold_dynamic
 
     mask = cv2.inRange(hsv, lower_color, upper_color)
@@ -149,11 +97,6 @@ def mask_image(hsv, color):
     return mask
 
 
-def find_angle(angle, a, b, c):
-    # dist = a*sin(b*angle+c)
-    return math.atan(a*b*math.cos(b*angle+c))
-
-
 def stereo_callback(data):
     global bridge
     global latest_pose
@@ -165,8 +108,8 @@ def stereo_callback(data):
     if debug_on:
         global gate_detection_dynamic_on
         this_pose = Pose()
-        # gate_detection_dynamic_on = True
-        gate_detection_dynamic_on = False
+        gate_detection_dynamic_on = True
+        # gate_detection_dynamic_on = False
     else:
         if latest_pose is None:
             print("No position")
@@ -627,7 +570,7 @@ def callback_dynamic_detection_changed(data):
 
 def callback_gate_size_changed(data):
     global gate_size
-    gate_size = data
+    gate_size = data.data
 
 
 if __name__ == '__main__':

scripts/main_navigation.py
@@ -40,6 +40,8 @@ def callback_visual_gate_dynamic_changed(input_data):
 
     measurement = input_data.data
 
+    rospy.loginfo("visual hand detected")
+
     if detection_dynamic_data.period is None:
         detection_dynamic_input_history = np.append(detection_dynamic_input_history,
                                                     [[measurement[0]], [measurement[1]]], axis=1)
@@ -89,7 +91,7 @@ def callback_visual_gate_dynamic_changed(input_data):
         else:
             rospy.loginfo("discard detected pointer")
 
-    rospy.loginfo("detection_dynamic_data.period")
+    rospy.loginfo("visual hand period")
     rospy.loginfo(detection_dynamic_data.period)
 
 
@@ -108,6 +110,8 @@ def callback_visual_gate_detection_changed(data):
         log_string = "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0"
         publisher_visual_log.publish(log_string)
         return
+
+    rospy.loginfo("visual gate detected")
 
     # read data
     bebop_position = data.bebop_pose.position
@@ -163,7 +167,7 @@ def callback_visual_gate_detection_changed(data):
     msg.hdg = wp_current.hdg
     publisher_wp_current.publish(msg)
 
-    rospy.loginfo("wp_current")
+    rospy.loginfo("new measured wp_current")
     rospy.loginfo(wp_current)
 
     # while there is no average yet: start collecting gate positions and evaluate standard deviation
@@ -209,7 +213,7 @@ def callback_visual_gate_detection_changed(data):
         # now, add to list only if gate position is close to last one
         distance = np.linalg.norm(np.array(gate_global_p) - wp_input_history[-1].pos)
         if distance < 1:
-            print("use detected gate")
+            rospy.loginfo("use detected gate")
             wp_input_history.append(wp_current)
             del wp_input_history[0]
             wp_average = cr.find_average(wp_input_history)
@@ -247,6 +251,8 @@ def callback_visual_gate_detection_changed(data):
         msg.hdg = wp_average.hdg
     publisher_wp_average.publish(msg)
 
+    rospy.loginfo("visual gate done")
+
     return
 
 
@@ -264,7 +270,7 @@ def calculate_visual_wp():
         # hover 4m behind dynamic gate at -0.5 height
         gate_pos = wp_average.pos
         gate_heading = wp_average.hdg
-        hover_distance = -4
+        hover_distance = -3.2
         hover_alt = -0.5
         extra_dist = np.array([hover_distance*math.cos(gate_heading), hover_distance*math.sin(gate_heading), hover_alt])
         wp_visual = cr.WP(gate_pos + extra_dist, gate_heading)
@@ -387,6 +393,28 @@ def calculate_blind_wp():
         wp_input_history = []
 
         rospy.loginfo("fly to turn2: " + str(wp_blind))
+
+
+    if state_auto == 71 and wp_blind is None:
+        rospy.loginfo("state 71, set new blind at to reach gate")
+
+        bebop_p = [bebop_position.x, bebop_position.y, bebop_position.z]
+        bebop_q = [bebop_orientation.x, bebop_orientation.y, bebop_orientation.z, bebop_orientation.w]
+
+        blind_position = [0.0, 0.0, 0.5]  # front, left , up
+        blind_position_global = cr.qv_mult(bebop_q, blind_position) + bebop_p
+        blind_position_global = blind_position_global.tolist()
+
+        look_position = [5, 0, 0]  # front, left, up
+        look_position_global = cr.qv_mult(bebop_q, look_position) + bebop_p
+        look_position_global = look_position_global.tolist()
+
+        # initialize with this position
+        wp_blind = cr.WP(blind_position_global, 0)
+        rospy.loginfo("global position set for blind")
+
+        wp_look = cr.WP(look_position_global, 0)
+
 
     rospy.loginfo("wp_blind")
     rospy.loginfo(wp_blind)
@@ -699,16 +727,23 @@ def navigate_dynamic():
             rospy.loginfo("DYN - yaw ok, state 0")
             detection_dynamic_data.state = 0
         else:
-            rospy.loginfo("DYN - correct yaw")
+            rospy.loginfo("DYN - yawing")
             msg.r = cr.limit_value(r_error, .1)
 
     # wait for gate rotation
     elif detection_dynamic_data.state == 2:
-        angle_difference = abs(detection_dynamic_data.theta-detection_dynamic_data.theta_trigger())
-        if angle_difference < abs(2*math.pi/(detection_dynamic_data.period*5)*.7):
-            rospy.loginfo("DYN - pointer angle triggered, state 3")
-            detection_dynamic_data.timer = time.time()
-            detection_dynamic_data.state = 3
+        if detection_dynamic_data.period is None:
+            rospy.loginfo("DYN - no period yet, state 0")
+            detection_dynamic_data.state = 0
+        else:
+            angle_difference = abs(detection_dynamic_data.theta-detection_dynamic_data.theta_trigger())
+            if angle_difference < abs(2*math.pi/(detection_dynamic_data.period*5)*.7):
+                rospy.loginfo("DYN - pointer angle triggered, state 3")
+                detection_dynamic_data.timer = time.time()
+                detection_dynamic_data.state = 3
+            else:
+                rospy.loginfo("DYN - wait for rotation")
+                detection_dynamic_data.state = 5
 
     # full forward
     elif detection_dynamic_data.state == 3:
@@ -718,7 +753,8 @@ def navigate_dynamic():
             detection_dynamic_data.timer = time.time()
         else:
             rospy.loginfo("DYN - full forward")
-            msg.x = 1
+            # msg.x = 1
+            detection_dynamic_data.state = 5
 
     # pause
     elif detection_dynamic_data.state == 4:
@@ -857,7 +893,7 @@ def state_machine_advancement(navigation_distance):
         rospy.loginfo("fly visual to front of G7")
         publisher_state_auto.publish(state_auto + 1)                   # 72 - fly visual to front of G7
 
-    elif state_auto == 72 and navigation_distance < 0.3:               # hovering in front of G7
+    elif state_auto == 72 and navigation_distance < 0.15:               # hovering in front of G7
         detection_active = True
         publisher_dynamic_detection_on.publish(True)
         nav_active = "dynamic"
43bca9e3c70c4775e5d8ed89e91faa0f6bf1f7a1
Vincenz Lenny <vincenz-frenzel@gmx.de>
2018-09-05 13:15:36 -0400

    minor changes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# rqt_gcs_gui requires rqt_gui
-sudo apt-get install ros-kinetic-rqt ros-kinetic-rqt-common-plugins 
-
 # bebop_auto requirements
-sudo apt-get install ros-kinetic-robot-upstart 
 sudo apt-get install ros-kinetic-rviz
 sudo apt-get install ros-kinetic-robot-state-publisher
 sudo apt-get install python-tf
@@ -12,9 +8,11 @@ sudo apt-get install python-pygame
 sudo apt-get install ros-kinetic-tf
 sudo python -m pip install https://pypi.python.org/packages/source/g/getch/getch-1.0-python2.tar.gz
 
-
+# bebop_autonomy
+sudo apt-get install build-essential python-rosdep python-catkin-tools
 
 # run catkin_make in workspace
 sudo ~/catkin_ws/catkin_make
 # close all terminals
 exit
+

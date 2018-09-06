9c35b50f07341c783467cd0dad1c4e3975ecc746
derekbt96 <derekbt@yahoo.com>
2018-09-05 18:49:53 -0700

    Matlab and ground input change

--- Matlab/ROS_data_grabber.m
+++ Matlab/ROS_data_grabber.m
@@ -1,5 +1,6 @@
-% rosshutdown
-% rosinit('192.168.1.2')
+rosshutdown
+rosinit('192.168.1.2')
+
 
 global NavPID_data
 global NavPID_indx
@@ -7,25 +8,6 @@ global temp_index_Nav
 global Gate_data
 global Gate_indx
 global temp_index_Gate
-
-
-if ~exist('NavPID_indx')
-    load('NavPID.mat','NavPID_data','NavPID_indx')
-    if ~exist('NavPID_indx')
-        NavPID_indx = 0;
-        NavPID_data = cell(1000,1000);
-        display('starting NavPID over')
-    end
-end
-
-if ~exist('Gate_indx')
-    load('GateData.mat','Gate_data','Gate_indx')
-    if ~exist('Gate_indx')
-        Gate_indx = 0;
-        Gate_data = cell(1000,1000);
-        display('starting Gate over')
-    end
-end
 
 
 NavPID_indx = NavPID_indx + 1;
@@ -37,7 +19,6 @@ temp_index_Gate = 1;
 sub_nav = rossubscriber('/auto/navigation_logger','std_msgs/String',@nav_callback);
 sub_gate = rossubscriber('/auto/visual_logger','std_msgs/String',@gate_callback);
 fprintf('connected, recording on index %d and %d \n',NavPID_indx, Gate_indx)
-
 
 
 
@@ -53,6 +34,7 @@ fprintf('Saved the data')
 %%
 load('NavPID.mat','NavPID_data','NavPID_indx')
 load('GateData.mat','Gate_data','Gate_indx')
+fprintf('Data loaded')
 
 %%
 NavPID_indx = NavPID_indx - 1
@@ -61,6 +43,17 @@ Gate_indx = Gate_indx - 1
 %%
 NavPID_indx = NavPID_indx + 1
 Gate_indx = Gate_indx + 1
+
+%%
+
+
+
+NavPID_indx = 0;
+NavPID_data = cell(1000,10000);
+
+Gate_indx = 0;
+Gate_data = cell(1000,10000);
+
 
 %%
 
@@ -75,6 +68,9 @@ function gate_callback(src, msg)
     global Gate_data
     global Gate_indx
     global temp_index_Gate
-    Gate_data{Gate_indx,temp_index_Gate} = cellstr(msg2.Data);
-    temp_index_Gate = temp_index_Gate+1;
+    temp = strsplit(msg.Data,', ');
+    if temp{1} ~= '0'
+        Gate_data{Gate_indx,temp_index_Gate} = cellstr(msg.Data);
+        temp_index_Gate = temp_index_Gate+1;
+    end
 end
--- Matlab/ROS_display.m
+++ Matlab/ROS_display.m
@@ -1,28 +1,56 @@
-%%
-% 1 wp_current.pos[0]
-% 2 wp_current.pos[1]
-% 3 wp_current.pos[2]
-% 4 wp_current.hdg
-% 5 wp_average.pos[0]
-% 6 wp_average.pos[1]
-% 7 wp_average.pos[2]
-% 8 wp_average.hdg
-% 9 bebop_p[0][0]
-% 10 bebop_p[1][0]
-% 11 bebop_p[2][0]
-% 12 bebop_q[3]
-% 13 bebop_q[0]
-% 14 bebop_q[1]
-% 15 bebop_q[2]
-% 16 heading_to_gate
-                     
-%% X Total
-dataTemp2 = dataVar(indx,:);
+% x_pos_error 
+% x_vel_des[0] 
+% x_vel_des[1] 
+% x_vel_des[2] 
+% sum(x_vel_des) 
+% global_vel[0] 
+% x_vel_error 
+% nav_cmd_x[0] 
+% nav_cmd_x[1] 
+% nav_cmd_x[2] 
+% sum(nav_cmd_x) 
+% msg.x 
+% y_pos_error 
+% y_vel_des[0] 
+% y_vel_des[1] 
+% y_vel_des[2] 
+% sum(y_vel_des) 
+% global_vel[1] 
+% y_vel_error 
+% nav_cmd_y[0] 
+% nav_cmd_y[1] 
+% nav_cmd_y[2] 
+% sum(nav_cmd_y) 
+% msg.y 
+% diff_global[2] 
+% z_error 
+% nav_cmd_z[0] 
+% nav_cmd_z[1] 
+% nav_cmd_z[2] 
+% sum(nav_cmd_z) 
+% msg.z 
+% pos_theta 
+% angle 
+% r_error 
+% nav_cmd_r[0] 
+% nav_cmd_r[1] 
+% nav_cmd_r[2] 
+% sum(nav_cmd_r))
+% msg.r
+% time.time()
+% 0
+% 0
+% 0
+% 0
+
+%% X and Y Position
+
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
        break 
     end
 end
@@ -30,27 +58,64 @@ end
 figure
 hold on
 grid on
-plot(dataTemp(:,1))
-plot(dataTemp(:,2))
-plot(dataTemp(:,3))
-plot(dataTemp(:,4))
-plot(dataTemp(:,5))
-plot(dataTemp(:,6))
-plot(dataTemp(:,7))
-plot(dataTemp(:,8))
-plot(dataTemp(:,9))
-plot(dataTemp(:,10))
-plot(dataTemp(:,11))
-plot(dataTemp(:,12))
+plot(dataTemp(:,40),dataTemp(:,1))
+plot(dataTemp(:,40),dataTemp(:,2))
+plot(dataTemp(:,40),dataTemp(:,3))
+plot(dataTemp(:,40),dataTemp(:,4))
+plot(dataTemp(:,40),dataTemp(:,5))
+plot(dataTemp(:,40),dataTemp(:,6))
+legend('X pos error','X Pos P','X Pos I','X Pos D','X Vel Des','Vel x')
+title('Position X')
+
+figure
+hold on
+grid on
+plot(dataTemp(:,40),dataTemp(:,13))
+plot(dataTemp(:,40),dataTemp(:,14))
+plot(dataTemp(:,40),dataTemp(:,15))
+plot(dataTemp(:,40),dataTemp(:,16))
+plot(dataTemp(:,40),dataTemp(:,17))
+plot(dataTemp(:,40),dataTemp(:,18))
+legend('Y Pos Error','Y Pos P','Y Pos I','Y Pos D','Y Vel Des','Vel Y')
+title('Position Y')
+
+
+%% X Total
+dataTemp2 = NavPID_data(NavPID_indx,:);
+dataTemp = [];
+for k = 1:1000
+    dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
+    
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
+       break 
+    end
+end
+
+figure
+hold on
+grid on
+% plot(dataTemp(:,40)-dataTemp(1,40),dataTemp(:,1))
+plot(dataTemp(:,40),dataTemp(:,1))
+plot(dataTemp(:,40),dataTemp(:,2))
+plot(dataTemp(:,40),dataTemp(:,3))
+plot(dataTemp(:,40),dataTemp(:,4))
+plot(dataTemp(:,40),dataTemp(:,5))
+plot(dataTemp(:,40),dataTemp(:,6))
+plot(dataTemp(:,40),dataTemp(:,7))
+plot(dataTemp(:,40),dataTemp(:,8))
+plot(dataTemp(:,40),dataTemp(:,9))
+plot(dataTemp(:,40),dataTemp(:,10))
+plot(dataTemp(:,40),dataTemp(:,11))
+plot(dataTemp(:,40),dataTemp(:,12))
 legend('x pos error','X Pos P','X Pos I','X Pos D','X Vel Des','Vel x','X vel error','X Vel P','X Vel I','X Vel D','X Vel Out','msg.x')
 
 %% X Pos/Vel
-dataTemp2 = dataVar(indx,:);
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
        break 
     end
 end
@@ -58,12 +123,12 @@ end
 figure
 hold on
 grid on
-plot(dataTemp(:,1))
-plot(dataTemp(:,2))
-plot(dataTemp(:,3))
-plot(dataTemp(:,4))
-plot(dataTemp(:,5))
-plot(dataTemp(:,6))
+plot(dataTemp(:,40),dataTemp(:,1))
+plot(dataTemp(:,40),dataTemp(:,2))
+plot(dataTemp(:,40),dataTemp(:,3))
+plot(dataTemp(:,40),dataTemp(:,4))
+plot(dataTemp(:,40),dataTemp(:,5))
+plot(dataTemp(:,40),dataTemp(:,6))
 legend('X pos error','X Pos P','X Pos I','X Pos D','X Vel Des','Vel x')
 title('Position')
 
@@ -71,24 +136,24 @@ title('Position')
 figure
 hold on
 grid on
-plot(dataTemp(:,5))
-plot(dataTemp(:,6))
-plot(dataTemp(:,7))
-plot(dataTemp(:,8))
-plot(dataTemp(:,9))
-plot(dataTemp(:,10))
-plot(dataTemp(:,11))
-plot(dataTemp(:,12))
+plot(dataTemp(:,40),dataTemp(:,5),'o-')
+plot(dataTemp(:,40),dataTemp(:,6))
+plot(dataTemp(:,40),dataTemp(:,7))
+plot(dataTemp(:,40),dataTemp(:,8))
+plot(dataTemp(:,40),dataTemp(:,9))
+plot(dataTemp(:,40),dataTemp(:,10))
+plot(dataTemp(:,40),dataTemp(:,11))
+plot(dataTemp(:,40),dataTemp(:,12))
 legend('X Vel Des','Vel x','X vel error','X Vel P','X Vel I','X Vel D','X Vel Out','msg.x')
 title('Velocity')
 
 %% Y Total
-dataTemp2 = dataVar(indx,:);
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
        break 
     end
 end
@@ -96,30 +161,30 @@ end
 figure
 hold on
 grid on
-plot(dataTemp(:,13))
-plot(dataTemp(:,14))
-plot(dataTemp(:,15))
-plot(dataTemp(:,16))
-plot(dataTemp(:,17))
-plot(dataTemp(:,18))
-plot(dataTemp(:,19))
-plot(dataTemp(:,20))
-plot(dataTemp(:,21))
-plot(dataTemp(:,22))
-plot(dataTemp(:,23))
-plot(dataTemp(:,24))
+plot(dataTemp(:,40),dataTemp(:,13))
+plot(dataTemp(:,40),dataTemp(:,14))
+plot(dataTemp(:,40),dataTemp(:,15))
+plot(dataTemp(:,40),dataTemp(:,16))
+plot(dataTemp(:,40),dataTemp(:,17))
+plot(dataTemp(:,40),dataTemp(:,18))
+plot(dataTemp(:,40),dataTemp(:,19))
+plot(dataTemp(:,40),dataTemp(:,20))
+plot(dataTemp(:,40),dataTemp(:,21))
+plot(dataTemp(:,40),dataTemp(:,22))
+plot(dataTemp(:,40),dataTemp(:,23))
+plot(dataTemp(:,40),dataTemp(:,24))
 legend('Y Pos Error','Y Pos P','Y Pos I','Y Pos D','Y Vel Des','Vel Y','Y Vel Error','Y Vel P','Y Vel I','Y Vel D','Y Vel Out','msg.y')
 
 
 
 
 %% Y Pos/Vel
-dataTemp2 = dataVar(indx,:);
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
        break 
     end
 end
@@ -127,58 +192,59 @@ end
 figure
 hold on
 grid on
-plot(dataTemp(:,13))
-plot(dataTemp(:,14))
-plot(dataTemp(:,15))
-plot(dataTemp(:,16))
-plot(dataTemp(:,17))
-plot(dataTemp(:,18))
+plot(dataTemp(:,40),dataTemp(:,13))
+plot(dataTemp(:,40),dataTemp(:,14))
+plot(dataTemp(:,40),dataTemp(:,15))
+plot(dataTemp(:,40),dataTemp(:,16))
+plot(dataTemp(:,40),dataTemp(:,17))
+plot(dataTemp(:,40),dataTemp(:,18))
 legend('Y Pos Error','Y Pos P','Y Pos I','Y Pos D','Y Vel Des','Vel Y')
 title('Position')
 
 figure
 hold on
 grid on
-plot(dataTemp(:,18))
-plot(dataTemp(:,19))
-plot(dataTemp(:,20))
-plot(dataTemp(:,21))
-plot(dataTemp(:,22))
-plot(dataTemp(:,23))
-plot(dataTemp(:,24))
-legend('Vel Y','Y Vel Error','Y Vel P','Y Vel I','Y Vel D','Y Vel Out','msg.y')
+plot(dataTemp(:,40),dataTemp(:,17),'o-')
+plot(dataTemp(:,40),dataTemp(:,18))
+plot(dataTemp(:,40),dataTemp(:,19))
+plot(dataTemp(:,40),dataTemp(:,20))
+plot(dataTemp(:,40),dataTemp(:,21))
+plot(dataTemp(:,40),dataTemp(:,22))
+plot(dataTemp(:,40),dataTemp(:,23))
+plot(dataTemp(:,40),dataTemp(:,24))
+legend('Y Vel Des','Vel Y','Y Vel Error','Y Vel P','Y Vel I','Y Vel D','Y Vel Out','msg.y')
 title('Velocity')
 
 
 %% Z
-dataTemp2 = dataVar(indx,:);
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
-       break 
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
+       break
     end
 end
 
 figure
 hold on
 grid on
-plot(dataTemp(:,26))
-plot(dataTemp(:,27))
-plot(dataTemp(:,28))
-plot(dataTemp(:,29))
-plot(dataTemp(:,30))
-plot(dataTemp(:,31))
+plot(dataTemp(:,40),dataTemp(:,26),'-o')
+plot(dataTemp(:,40),dataTemp(:,27))
+plot(dataTemp(:,40),dataTemp(:,28))
+plot(dataTemp(:,40),dataTemp(:,29))
+plot(dataTemp(:,40),dataTemp(:,30))
+plot(dataTemp(:,40),dataTemp(:,31))
 legend('Z Error','Z Pos P','Z Pos I','D term','Z Vel Out','msg.z')
 
 %% R
-dataTemp2 = dataVar(indx,:);
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
        break 
     end
 end
@@ -186,33 +252,33 @@ end
 figure
 hold on
 grid on
-plot(dataTemp(:,32))
-plot(dataTemp(:,33))
-plot(dataTemp(:,34))
-plot(dataTemp(:,35))
-plot(dataTemp(:,36))
-plot(dataTemp(:,37))
-plot(dataTemp(:,38))
-plot(dataTemp(:,39))
+plot(dataTemp(:,40),dataTemp(:,32))
+plot(dataTemp(:,40),dataTemp(:,33))
+plot(dataTemp(:,40),dataTemp(:,34))
+plot(dataTemp(:,40),dataTemp(:,35))
+plot(dataTemp(:,40),dataTemp(:,36))
+plot(dataTemp(:,40),dataTemp(:,37))
+plot(dataTemp(:,40),dataTemp(:,38))
+plot(dataTemp(:,40),dataTemp(:,39))
 legend('Angle Des','Vehicle Angle','R error','P term','I term','D term','nav cmd R','msg.r')
 
 
 %% mics
-dataTemp2 = dataVar(indx,:);
+dataTemp2 = NavPID_data(NavPID_indx,:);
 dataTemp = [];
 for k = 1:1000
     dataTemp = [dataTemp;cellfun(@str2num,strsplit(dataTemp2{k}{1},', '))];
     
-    if ~iscell(dataVar{indx,k+1})
+    if ~iscell(NavPID_data{NavPID_indx,k+1})
        break 
     end
 end
 figure
 hold on
 grid on
-plot(dataTemp(:,40))
-plot(dataTemp(:,41))
-plot(dataTemp(:,42))
-plot(dataTemp(:,43))
-plot(dataTemp(:,44))
+plot(dataTemp(:,40),dataTemp(:,40))
+plot(dataTemp(:,40),dataTemp(:,41))
+plot(dataTemp(:,40),dataTemp(:,42))
+plot(dataTemp(:,40),dataTemp(:,43))
+plot(dataTemp(:,40),dataTemp(:,44))
 legend('1','2','3','4','5')
